a6387f279ed50585750a280beddbefc96ff58339
Keegan <mhsjlw@aol.com>
2016-03-06 11:59:26 -0500

    metadata!

--- src/datatypes.js
+++ src/datatypes.js
@@ -27,7 +27,45 @@ function sizeOfNbt(value) {
   return nbt.proto.sizeOf(value,"nbt");
 }
 
+function readEntityMetadata(buffer, offset, {type,endVal}) {
+  let cursor = offset;
+  const metadata = [];
+  let item;
+  while(true) {
+    if(offset+1>buffer.length)
+      throw new PartialReadError();
+    item = buffer.readUInt8(cursor);
+    if(item === endVal) {
+      return {
+        value: metadata,
+        size: cursor + 1 - offset
+      };
+    }
+    const results = this.read(buffer, cursor, type, {});
+    metadata.push(results.value);
+    cursor += results.size;
+  }
+}
+
+function writeEntityMetadata(value, buffer, offset, {type,endVal}) {
+  const self = this;
+  value.forEach(function(item) {
+    offset = self.write(item, buffer, offset, type, {});
+  });
+  buffer.writeUInt8(endVal, offset);
+  return offset + 1;
+}
+
+function sizeOfEntityMetadata(value, {type}) {
+  let size = 1;
+  for(let i = 0; i < value.length; ++i) {
+    size += this.sizeOf(value[i], type, {});
+  }
+  return size;
+}
+
 module.exports = {
   'uuid': [readUUID, writeUUID, 16],
-  'nbt': [readNbt, writeNbt, sizeOfNbt]
+  'nbt': [readNbt, writeNbt, sizeOfNbt],
+  'entityMetadataLoop': [readEntityMetadata, writeEntityMetadata, sizeOfEntityMetadata]
 };
