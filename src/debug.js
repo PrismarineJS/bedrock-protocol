1b23ccfcf64ae338495c32d9aa3db54b8c70a9df
mhsjlw <mhsjlw@users.noreply.github.com>
2016-04-02 15:56:47 -0400

    update deps

--- src/debug.js
+++ src/debug.js
@@ -4,15 +4,13 @@ var debug;
 if(process.env.NODE_DEBUG) {
   var pid = process.pid;
   debug = function(x) {
-    // if console is not set up yet, then skip this.
     if(!console.error)
       return;
-    console.error('MC-PROTO: %d', pid,
+    console.error('MCPE-PROTO: %d', pid,
       util.format.apply(util, arguments));
   };
 } else {
-  debug = function() {
-  };
+  debug = function() {}
 }
 
 module.exports = debug;
