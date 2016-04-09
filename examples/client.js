9b0a7130500c4e29f70327389c8414ceb10c4afb
Romain Beaumont <romain.rom1@gmail.com>
2016-04-09 21:55:45 +0200

    ask for chunks in the client.js example and update raknet

--- examples/client.js
+++ examples/client.js
@@ -13,6 +13,12 @@ var client = pmp.createClient({
 
 client.on('mcpe',packet => console.log(packet));
 
+client.on('mcpe_set_spawn_position',() => {
+  client.writeMCPE('mcpe_request_chunk_radius',{
+    chunk_radius:8
+  })
+});
+
 client.on('error',function(err){
   console.log(err);
 });
