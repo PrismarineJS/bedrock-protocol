97c8b7d0aa65b6de384ebfd1c14ce9c3f230863c
mhsjlw <mhsjlw@users.noreply.github.com>
2016-06-14 18:40:20 -0400

    change packet prefix, remove duplicate packets

--- examples/server.js
+++ examples/server.js
@@ -17,83 +17,84 @@ var server = pmp.createServer({
 server.on('connection', function(client) {
 
 
-  client.on("mcpe",packet => console.log(packet));
+  client.on("mcpe",packet => console.log(JSON.stringify(packet)));
 
   client.on("game_login",packet => {
-    client.writeMCPE("player_status",{
-      status:0
-    });
-    client.writeMCPE('move_player', {
-      entityId: [0,0],
-      x: 1,
-      y: 64 + 1.62,
-      z: 1,
-      yaw: 0,
-      headYaw: 0,
-      pitch: 0,
-      mode: 0,
-      onGround: 1
-    });
-
-    client.writeMCPE("start_game",{
-      seed:-1,
-      dimension:0,
-      generator:1,
-      gamemode:1,
-      entityId:[0,0],
-      spawnX:1,
-      spawnY:1,
-      spawnZ:1,
-      x:0,
-      y:1+1.62,
-      z:0,
-      isLoadedInCreative:false,
-      dayCycleStopTime:0,
-      eduMode:false,
-      unknown:""
-    });
-
-    client.writeMCPE('set_spawn_position', {
-      x: 1,
-      y: 64,
-      z: 1
-    });
-    client.writeMCPE("set_time",{
-      time:0,
-      started:1
-    });
-
-    client.writeMCPE('respawn', {
-      x: 1,
-      y: 64,
-      z: 1
-    });
-  });
-
-  client.on("request_chunk_radius",() => {
-    client.writeMCPE('chunk_radius_update',{
-      chunkRadius:1
-    });
-
-    for (let x = -1; x <=1; x++) {
-      for (let z = -1; z <=1; z++) {
-        client.writeBatch([{"name":"mcpe","params":{name:"full_chunk_data",params:{
-        chunkX: x,
-        chunkZ: z,
-        order: 1,
-        chunk_data:fs.readFileSync(__dirname+"/chunk")
-        }}}]);
-      }
-    }
-
-    client.writeMCPE('player_status', {
-      status: 3
-    });
-
-    client.writeMCPE('set_time', {
-      time: 0,
-      started: 1
-    });
+    console.log(packet);
+  //   client.writeMCPE("player_status",{
+  //     status:0
+  //   });
+  //   client.writeMCPE('move_player', {
+  //     entityId: [0,0],
+  //     x: 1,
+  //     y: 64 + 1.62,
+  //     z: 1,
+  //     yaw: 0,
+  //     headYaw: 0,
+  //     pitch: 0,
+  //     mode: 0,
+  //     onGround: 1
+  //   });
+  //
+  //   client.writeMCPE("start_game",{
+  //     seed:-1,
+  //     dimension:0,
+  //     generator:1,
+  //     gamemode:1,
+  //     entityId:[0,0],
+  //     spawnX:1,
+  //     spawnY:1,
+  //     spawnZ:1,
+  //     x:0,
+  //     y:1+1.62,
+  //     z:0,
+  //     isLoadedInCreative:false,
+  //     dayCycleStopTime:0,
+  //     eduMode:false,
+  //     unknown:""
+  //   });
+  //
+  //   client.writeMCPE('set_spawn_position', {
+  //     x: 1,
+  //     y: 64,
+  //     z: 1
+  //   });
+  //   client.writeMCPE("set_time",{
+  //     time:0,
+  //     started:1
+  //   });
+  //
+  //   client.writeMCPE('respawn', {
+  //     x: 1,
+  //     y: 64,
+  //     z: 1
+  //   });
+  // });
+  //
+  // client.on("request_chunk_radius",() => {
+  //   client.writeMCPE('chunk_radius_update',{
+  //     chunkRadius:1
+  //   });
+  //
+  //   for (let x = -1; x <=1; x++) {
+  //     for (let z = -1; z <=1; z++) {
+  //       client.writeBatch([{"name":"mcpe","params":{name:"full_chunk_data",params:{
+  //       chunkX: x,
+  //       chunkZ: z,
+  //       order: 1,
+  //       chunk_data:fs.readFileSync(__dirname+"/chunk")
+  //       }}}]);
+  //     }
+  //   }
+  //
+  //   client.writeMCPE('player_status', {
+  //     status: 3
+  //   });
+  //
+  //   client.writeMCPE('set_time', {
+  //     time: 0,
+  //     started: 1
+  //   });
 
   });
 
