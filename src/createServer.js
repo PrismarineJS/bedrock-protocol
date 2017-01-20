5c6042b61f3ba351033c43bba9a1c53aa4e482de
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2017-01-20 09:21:05 +0100

    *faceplam*

--- src/createServer.js
+++ src/createServer.js
@@ -80,7 +80,7 @@ function createServer(options, encryption) {
         // array: packets to send
         client.writeBatch = function (packets) {
             const payload = zlib.deflateSync(batchProto.createPacketBuffer('insideBatch',
-                packets.map(packet => client.mcpePacketSerializer.createPacketBuffer(packets))));
+                packets.map(packets => client.mcpePacketSerializer.createPacketBuffer(packets))));
 
             client.writePacket('batch', {
                 payload: payload
