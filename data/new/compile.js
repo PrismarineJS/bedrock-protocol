73d4dd1325efc7d6f51d42dfab494ed74a5e7126
extremeheat <extreme@protonmail.ch>
2021-02-07 05:25:22 -0500

    protocol def updates

---
+++ data/new/compile.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+let compile
+try {
+    compile = require('protodef-yaml/compiler')
+} catch (e) {
+    require('child_process').execSync('npx protodef-yaml proto.yml protocol.json')
+}
+
+if (compile) {
+    compile('./proto.yml', 'protocol.json')
+}
+
+fs.writeFileSync( '../newproto.json', JSON.stringify({ types: require('./protocol.json') }, null, 2) )
+fs.unlinkSync('./protocol.json') //remove temp file
