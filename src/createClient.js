2b7833310c0e1d8425af94a1f108b89fd9b2a335
extremeheat <extreme@protonmail.ch>
2021-04-16 18:23:18 -0400

    fix createClient keepalive

--- src/createClient.js
+++ src/createClient.js
@@ -43,6 +43,7 @@ function connect (client) {
 
     client.queue('client_cache_status', { enabled: false })
     client.queue('request_chunk_radius', { chunk_radius: client.renderDistance || 1 })
+    client.queue('tick_sync', { request_time: BigInt(Date.now()), response_time: 0n })
   })
 
   const KEEPALIVE_INTERVAL = 10 // Send tick sync packets every 10 ticks
