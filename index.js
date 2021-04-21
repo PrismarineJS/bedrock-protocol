95decd7942e2116532cc9b0f197abc40fd85e4ec
extremeheat <extreme@protonmail.ch>
2021-04-21 07:07:26 -0400

    Add version check

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+  console.error('Your node version is currently', process.versions.node)
+  console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
+  process.exit(1)
+}
+
+const { Client } = require('./src/client')
+const { Server } = require('./src/server')
+const { Relay } = require('./src/relay')
+const { createClient, ping } = require('./src/createClient')
+const { createServer } = require('./src/createClient')
+
 module.exports = {
-  ...require('./src/client'),
-  ...require('./src/server'),
-  ...require('./src/serverPlayer'),
-  ...require('./src/relay'),
-  ...require('./src/createClient'),
-  ...require('./src/createServer')
+  Client,
+  Server,
+  Relay,
+  createClient,
+  ping,
+  createServer
 }
