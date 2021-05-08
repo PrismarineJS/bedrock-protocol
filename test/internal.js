8ba2b890c25035116c554467a59c5bf21e86cf8e
extremeheat <extreme@protonmail.ch>
2021-05-08 04:28:27 -0400

    enable debugging output in tests

--- test/internal.js
+++ test/internal.js
@@ -1,4 +1,4 @@
-// process.env.DEBUG = 'minecraft-protocol raknet'
+process.env.DEBUG = 'minecraft-protocol raknet'
 const { Server, Client } = require('../')
 const { dumpPackets } = require('../tools/genPacketDumps')
 const DataProvider = require('../data/provider')
