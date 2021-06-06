8daa43715d40a81b515471179a0c1bc31f9d7ccb
extremeheat <extreme@protonmail.ch>
2021-06-06 03:39:24 +0000

    fix lint

--- tools/generateDocs.js
+++ tools/generateDocs.js
@@ -1,25 +1,24 @@
 const fs = require('fs')
-const { Versions } = require('../src/options')
+// const { Versions } = require('../src/options')
 const { join } = require('path')
 const { parse, genHTML } = require('protodef-yaml')
 
-let inputFile, includeComments
-
 const ci = process.env.GITHUB_ACTIONS || process.argv[3]
 
-function createDocs() {
+function createDocs () {
   fs.writeFileSync('packet_map.yml', '!import: types.yaml')
-  const inter = parse(inputFile = 'proto.yml', includeComments = true)
+  const inter = parse('proto.yml', true, true)
   const html = genHTML(inter, { includeHeader: true })
   if (ci) {
+    try { fs.mkdirSync('../publish') } catch {}
     const version = inter['!version']
-    fs.writeFileSync(`../${version}.html`, html)
+    fs.writeFileSync(`../publish/${version}.html`, html)
   }
 
   fs.unlinkSync('packet_map.yml')
 }
 
-function main(ver = 'latest') {
+function main (ver = 'latest') {
   process.chdir(join(__dirname, '/../data/', ver))
   console.log('p', process.cwd())
   if (!fs.existsSync('./proto.yml')) return
