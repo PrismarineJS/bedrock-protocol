22bb4fe7a1e74b4edd64bbfd721f2f59076d63cd
Jordan Jones <jordpjones@gmail.com>
2021-08-12 21:04:32 -0700

    implement prismarine-auth

--- src/client/auth.js
+++ src/client/auth.js
@@ -1,31 +1,44 @@
-const { MsAuthFlow } = require('./authFlow.js')
+const { Authflow: PrismarineAuth } = require('prismarine-auth')
+const minecraftFolderPath = require('minecraft-folder-path')
+const debug = require('debug')('minecraft-protocol')
 const { uuidFrom } = require('../datatypes/util')
 
 /**
- * Obtains Minecaft profile data using a Minecraft access token and starts the join sequence
+ * Authenticates to Minecraft via device code based Microsoft auth,
+ * then connects to the specified server in Client Options
+ *
+ * @function
  * @param {object} client - The client passed to protocol
  * @param {object} options - Client Options
- * @param {string} chains - Minecraft JWTs to send to server
  */
-async function postAuthenticate (client, options, chains) {
-  // First chain is Mojang stuff, second is Xbox profile data used by mc
-  const jwt = chains[1]
-  const [header, payload, signature] = jwt.split('.').map(k => Buffer.from(k, 'base64')) // eslint-disable-line
-  const xboxProfile = JSON.parse(String(payload))
-
-  // This profile / session here could be simplified down to where it just passes the uuid of the player to encrypt.js
-  // That way you could remove some lines of code. It accesses client.session.selectedProfile.id so /shrug.
-  // - Kashalls
-  const profile = {
-    name: xboxProfile?.extraData?.displayName || 'Player',
-    uuid: xboxProfile?.extraData?.identity || 'adfcf5ca-206c-404a-aec4-f59fff264c9b', // random
-    xuid: xboxProfile?.extraData?.XUID || 0
+async function authenticate (client, options) {
+  if (!options.profilesFolder) {
+    options.profilesFolder = minecraftFolderPath
   }
+  try {
+    const Authflow = new PrismarineAuth(options.username, options.profilesFolder, options, options.onMsaCode)
+    const chains = await Authflow.getMinecraftBedrockToken(client.clientX509)
 
-  client.profile = profile
-  client.username = profile.name
-  client.accessToken = chains
-  client.emit('session', profile)
+    debug('chains', chains)
+
+    // First chain is Mojang stuff, second is Xbox profile data used by mc
+    const jwt = chains[1]
+    const [header, payload, signature] = jwt.split('.').map(k => Buffer.from(k, 'base64')) // eslint-disable-line
+    const xboxProfile = JSON.parse(String(payload))
+
+    debug('got xbox profile', xboxProfile)
+
+    const profile = {
+      name: xboxProfile?.extraData?.displayName || 'Player',
+      uuid: xboxProfile?.extraData?.identity || 'adfcf5ca-206c-404a-aec4-f59fff264c9b', // random
+      xuid: xboxProfile?.extraData?.XUID || 0
+    }
+
+    return postAuthenticate(client, profile, chains)
+  } catch (err) {
+    console.error(err)
+    client.emit('error', err)
+  }
 }
 
 /**
@@ -38,49 +51,19 @@ function createOfflineSession (client, options) {
     uuid: uuidFrom(options.username), // random
     xuid: 0
   }
+  return postAuthenticate(client, profile, []) // No extra JWTs, only send 1 client signed chain with all the data
+}
+
+function postAuthenticate (client, profile, chains) {
   client.profile = profile
   client.username = profile.name
   client.accessToken = [] // No extra JWTs, only send 1 client signed chain with all the data
   client.emit('session', profile)
 }
 
-/**
- * Authenticates with Mincrosoft through user credentials, then
- * with Xbox Live, Minecraft, checks entitlements and returns profile
- *
- * @function
- * @param {object} client - The client passed to protocol
- * @param {object} options - Client Options
- */
-async function authenticatePassword (client, options) {
-  throw Error('Not implemented')
-}
-
-/**
- * Authenticates to Minecraft via device code based Microsoft auth,
- * then connects to the specified server in Client Options
- *
- * @function
- * @param {object} client - The client passed to protocol
- * @param {object} options - Client Options
- */
-async function authenticateDeviceCode (client, options) {
-  try {
-    const flow = new MsAuthFlow(options.username, options.profilesFolder, options, options.onMsaCode)
-
-    const chain = await flow.getMinecraftToken(client.clientX509)
-    // console.log('Chain', chain)
-    await postAuthenticate(client, options, chain)
-  } catch (err) {
-    console.error(err)
-    client.emit('error', err)
-  }
-}
-
 module.exports = {
   createOfflineSession,
-  authenticatePassword,
-  authenticateDeviceCode
+  authenticate
 }
 
 // async function msaTest () {
