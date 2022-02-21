233fccd0eb621b33a74e9d233c3877f4377c7f32
extremeheat <extreme@protonmail.ch>
2022-02-21 00:35:04 -0500

    re-enable proxy test on ubuntu runner

--- test/internal.test.js
+++ test/internal.test.js
@@ -17,17 +17,12 @@ describe('internal client/server test', function () {
     })
   }
 
-  if (process.env.CI && process.platform === 'linux') {
-    // Don't run the test, see :
-    // https://github.com/PrismarineJS/bedrock-protocol/issues/124
-  } else {
-    for (const version in Versions) {
-      it('proxies ' + version, async () => {
-        console.debug(version)
-        await proxyTest(version)
-        await sleep(5000)
-        console.debug('Done', version)
-      })
-    }
+  for (const version in Versions) {
+    it('proxies ' + version, async () => {
+      console.debug(version)
+      await proxyTest(version)
+      await sleep(5000)
+      console.debug('Done', version)
+    })
   }
 })
