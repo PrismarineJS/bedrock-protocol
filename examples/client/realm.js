b9a6edcc1c7fe0ee4dff8c2ced64758357925480
LucienHH <Lucien.holloway@aprox.co.uk>
2022-04-02 17:04:44 +0100

    Explain options

--- examples/client/realm.js
+++ examples/client/realm.js
@@ -2,9 +2,9 @@
 const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
   realms: {
-    // realmId: '1234567',
-    // realmInvite: 'https://realms.gg/AB1CD2EFA3B',
-    pickRealm: (realms) => realms.find(e => e.name === 'Realm Name')
+    // realmId: '1234567', // Connect the client to a Realm using the Realms ID
+    // realmInvite: 'https://realms.gg/AB1CD2EFA3B', // Connect the client to a Realm using the Realms invite URL or code
+    pickRealm: (realms) => realms.find(e => e.name === 'Realm Name') // Connect the client to a Realm using a function that returns a Realm
   }
 })
 
