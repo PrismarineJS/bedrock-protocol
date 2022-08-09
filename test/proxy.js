722e5e5f18426d3e77c0513c6c5843cdf2c94749
extremeheat <extreme@protonmail.ch>
2022-08-09 03:19:58 -0400

    change timeout

--- test/proxy.js
+++ test/proxy.js
@@ -1,7 +1,7 @@
 const { createClient, createServer, Relay } = require('bedrock-protocol')
 const { sleep, waitFor } = require('../src/datatypes/util')
 
-function proxyTest (version, raknetBackend = 'raknet-node', timeout = 1000 * 200) {
+function proxyTest (version, raknetBackend = 'raknet-node', timeout = 1000 * 40) {
   console.log('with raknet backend', raknetBackend)
   return waitFor(res => {
     const SERVER_PORT = 19000 + ((Math.random() * 100) | 0)
