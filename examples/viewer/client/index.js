8a5ba33f7c1633ca6965c0d5ace58c952cf932a6
extremeheat <extreme@protonmail.ch>
2022-09-22 22:24:24 -0400

    Remove viewer example

--- examples/viewer/client/index.js
+++
@@ -1,4 +0,0 @@
-const { BotViewer } = require('./BotViewer')
-
-global.viewer = new BotViewer()
-global.viewer.start()
--- examples/viewer/client/preload.js
+++
@@ -1,9 +0,0 @@
-// Required to detect electron in prismarine-viewer
-globalThis.isElectron = true
-
-// If you need to disable node integration:
-// * Node.js APIs will only be avaliable in this file
-// * Use this file to load a viewer manager class
-//   based on one of the examples
-// * Expose this class to the global window
-// * Interact with the class in your code
