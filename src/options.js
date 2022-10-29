e7f8357a36db5815b45855289362bf4299dc020e
extremeheat <extreme@protonmail.ch>
2022-10-29 02:25:31 -0400

    Support 1.19.40 (#314)

--- src/options.js
+++ src/options.js
@@ -3,7 +3,7 @@ const mcData = require('minecraft-data')
 // Minimum supported version (< will be kicked)
 const MIN_VERSION = '1.16.201'
 // Currently supported verson. Note, clients with newer versions can still connect as long as data is in minecraft-data
-const CURRENT_VERSION = '1.19.30'
+const CURRENT_VERSION = '1.19.40'
 
 const Versions = Object.fromEntries(mcData.versions.bedrock.filter(e => e.releaseType === 'release').map(e => [e.minecraftVersion, e.version]))
 
