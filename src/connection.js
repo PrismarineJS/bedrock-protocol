26b984b45a3a14a7c09c3c7a1d9eff0ccda24dd2
hvlxh <monsta22019@gmail.com>
2023-04-24 23:10:00 +0530

    Added any cases into sneak case in client write, queue.

--- src/connection.js
+++ src/connection.js
@@ -28,15 +28,24 @@ class Connection extends EventEmitter {
   }
 
   versionLessThan (version) {
-    return this.options.protocolVersion < (typeof version === 'string' ? Versions[version] : version)
+    return (
+      this.options.protocolVersion <
+      (typeof version === 'string' ? Versions[version] : version)
+    )
   }
 
   versionGreaterThan (version) {
-    return this.options.protocolVersion > (typeof version === 'string' ? Versions[version] : version)
+    return (
+      this.options.protocolVersion >
+      (typeof version === 'string' ? Versions[version] : version)
+    )
   }
 
   versionGreaterThanOrEqualTo (version) {
-    return this.options.protocolVersion >= (typeof version === 'string' ? Versions[version] : version)
+    return (
+      this.options.protocolVersion >=
+      (typeof version === 'string' ? Versions[version] : version)
+    )
   }
 
   startEncryption (iv) {
@@ -62,10 +71,42 @@ class Connection extends EventEmitter {
     }
   }
 
-  write (name, params) {
+  convertObjectKeysToUnderscore (obj) {
+    if (typeof obj !== 'object' || obj === null) {
+      return obj
+    }
+
+    if (Array.isArray(obj)) {
+      return obj.map((item) => this.convertObjectKeysToUnderscore(item))
+    }
+
+    const result = {}
+    for (const key of Object.keys(obj)) {
+      const value = obj[key]
+      const newKey = key.replace(
+        /[A-Z]/g,
+        (letter) => `_${letter.toLowerCase()}`
+      )
+      result[newKey] = this.convertObjectKeysToUnderscore(value)
+    }
+
+    return result
+  }
+
+  toSnakeCase (str) {
+    return str.replace(/[A-Z]/g, (match) => `_${match.toLowerCase()}`)
+  }
+
+  write (names, param) {
+    const params = this.convertObjectKeysToUnderscore(param)
+    const name = this.toSnakeCase(names)
     this.outLog?.(name, params)
     if (name === 'start_game') this.updateItemPalette(params.itemstates)
-    const batch = new Framer(this.compressionAlgorithm, this.compressionLevel, this.compressionThreshold)
+    const batch = new Framer(
+      this.compressionAlgorithm,
+      this.compressionLevel,
+      this.compressionThreshold
+    )
     const packet = this.serializer.createPacketBuffer({ name, params })
     batch.addEncodedPacket(packet)
 
@@ -91,7 +132,11 @@ class Connection extends EventEmitter {
 
   _tick () {
     if (this.sendQ.length) {
-      const batch = new Framer(this.compressionAlgorithm, this.compressionLevel, this.compressionThreshold)
+      const batch = new Framer(
+        this.compressionAlgorithm,
+        this.compressionLevel,
+        this.compressionThreshold
+      )
       batch.addEncodedPackets(this.sendQ)
       this.sendQ = []
       this.sendIds = []
@@ -115,7 +160,11 @@ class Connection extends EventEmitter {
    */
   sendBuffer (buffer, immediate = false) {
     if (immediate) {
-      const batch = new Framer(this.compressionAlgorithm, this.compressionLevel, this.compressionThreshold)
+      const batch = new Framer(
+        this.compressionAlgorithm,
+        this.compressionLevel,
+        this.compressionThreshold
+      )
       batch.addEncodedPacket(buffer)
       if (this.encryptionEnabled) {
         this.sendEncryptedBatch(batch)
@@ -139,7 +188,12 @@ class Connection extends EventEmitter {
   }
 
   sendMCPE (buffer, immediate) {
-    if (this.connection.connected === false || this.status === ClientStatus.Disconnected) return
+    if (
+      this.connection.connected === false ||
+      this.status === ClientStatus.Disconnected
+    ) {
+      return
+    }
     try {
       this.connection.sendReliable(buffer, immediate)
     } catch (e) {
@@ -162,8 +216,10 @@ class Connection extends EventEmitter {
     }
   }
 
-  handle (buffer) { // handle encapsulated
-    if (buffer[0] === 0xfe) { // wrapper
+  handle (buffer) {
+    // handle encapsulated
+    if (buffer[0] === 0xfe) {
+      // wrapper
       if (this.encryptionEnabled) {
         this.decrypt(buffer.slice(1))
       } else {
