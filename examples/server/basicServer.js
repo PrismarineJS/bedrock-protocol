7d665a9da4cabe6b08f3064a066d549be283e6b2
@kinash <92477814+andriycraft@users.noreply.github.com>
2023-05-10 10:04:34 +0200

    Update basicServer example to 1.19.80

--- examples/server/basicServer.js
+++ examples/server/basicServer.js
@@ -3,7 +3,7 @@ const bedrock = require('bedrock-protocol')
 const server = bedrock.createServer({
   host: '0.0.0.0',        // optional
   port: 19132,            // optional
-  version: '1.17.10',    // The server version
+  version: '1.19.80',    // The server version
   motd: {                 // The message of the day
     motd: 'Funtime Server',
     levelName: 'Wonderland'
@@ -12,7 +12,7 @@ const server = bedrock.createServer({
 
 server.on('connect', client => {
   client.on('join', () => { // The client has joined the server.
-    const d = new Date()  // Once client is in the server, send a colorful kick message
-    client.disconnect(`Good ${d.getHours() < 12 ? '§emorning§r' : '§3afternoon§r'} :)\n\nMy time is ${d.toLocaleString()} !`)
+    const date = new Date()  // Once client is in the server, send a colorful kick message
+    client.disconnect(`Good ${date.getHours() < 12 ? '§emorning§r' : '§3afternoon§r'}\n\nMy time is ${date.toLocaleString()} !`)
   })
 })
