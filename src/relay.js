a499d0b25c92477943dffb319de3416e02b2505e
MrSterdy <mrsterdy@gmail.com>
2023-07-22 22:40:30 +0400

    Add disconnectOnParseError option

--- src/relay.js
+++ src/relay.js
@@ -49,6 +49,11 @@ class RelayPlayer extends Player {
     } catch (e) {
       this.server.deserializer.dumpFailedBuffer(packet, this.connection.address)
       console.error(this.connection.address, e)
+
+      if (this.options.disconnectOnParseError) {
+        this.disconnect('Server packet parse error')
+      }
+
       return
     }
     const name = des.data.name
