2efafd9dd39df81b7693a740b4a81c10b42012c8
MrSterdy <mrsterdy@gmail.com>
2023-07-22 21:14:03 +0400

    Remove disconnect on upstream packet deserialization error

--- src/relay.js
+++ src/relay.js
@@ -49,7 +49,6 @@ class RelayPlayer extends Player {
     } catch (e) {
       this.server.deserializer.dumpFailedBuffer(packet, this.connection.address)
       console.error(this.connection.address, e)
-      this.disconnect('Server packet parse error')
       return
     }
     const name = des.data.name
