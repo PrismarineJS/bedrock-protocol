a7a86b858db329db76a95a8b0dd3a78134241819
unknown <lucienharryholloway@hotmail.co.uk>
2024-10-04 01:08:24 +0100

    Implement Nethernet spec

---
+++ src/nethernet/discovery/packets/MessagePacket.js
@@ -0,0 +1,31 @@
+const { PACKET_TYPE, Packet } = require('./packet')
+
+class MessagePacket extends Packet {
+  constructor (data) {
+    super(PACKET_TYPE.DISCOVERY_MESSAGE, data)
+  }
+
+  encode () {
+    super.encode()
+    this.writeUnsignedLongLE(this.recipientId)
+
+    this.writeUnsignedIntLE(this.data.length)
+    this.write(Buffer.from(this.data, 'utf-8'))
+
+    this.prependLength()
+
+    return this
+  }
+
+  decode () {
+    super.decode()
+    this.recipientId = this.readUnsignedLongLE()
+
+    const length = this.readUnsignedIntLE()
+    this.data = this.read(length).toString()
+
+    return this
+  }
+}
+
+module.exports = { MessagePacket }
