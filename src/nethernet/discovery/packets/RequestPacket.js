d85096221f8adfe5b4b0940c964c0ae101e45cac
unknown <lucienharryholloway@hotmail.co.uk>
2024-10-04 01:08:24 +0100

    Implement Nethernet spec

---
+++ src/nethernet/discovery/packets/RequestPacket.js
@@ -0,0 +1,23 @@
+const { PACKET_TYPE, Packet } = require('./packet')
+
+class RequestPacket extends Packet {
+  constructor (data) {
+    super(PACKET_TYPE.DISCOVERY_REQUEST, data)
+  }
+
+  encode () {
+    super.encode()
+
+    this.prependLength()
+
+    return this
+  }
+
+  decode () {
+    super.decode()
+
+    return this
+  }
+}
+
+module.exports = { RequestPacket }
