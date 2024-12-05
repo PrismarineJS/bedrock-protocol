7c7b402365965b46dfddc3189fb52a4037d3281b
extremeheat <extreme@protonmail.ch>
2024-12-04 23:53:38 -0500

    lint

--- tools/startVanillaServer.js
+++ tools/startVanillaServer.js
@@ -2,10 +2,10 @@ const bedrockServer = require('minecraft-bedrock-server')
 
 module.exports = {
   ...bedrockServer,
-  startServerAndWait(version, withTimeout, options) {
+  startServerAndWait (version, withTimeout, options) {
     return bedrockServer.startServerAndWait(version, withTimeout, { ...options, root: __dirname })
   },
-  startServerAndWait2(version, withTimeout, options) {
+  startServerAndWait2 (version, withTimeout, options) {
     return bedrockServer.startServerAndWait2(version, withTimeout, { ...options, root: __dirname })
   }
 }
