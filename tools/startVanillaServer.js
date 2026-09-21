c4d5aca5e3529ec00e55919c619c876efae78c4e
extremeheat <extreme@protonmail.ch>
2026-09-21 05:15:09 +0000

    Keep vanilla schema tests on RakNet when BDS defaults to Nethernet

--- tools/startVanillaServer.js
+++ tools/startVanillaServer.js
@@ -1,11 +1,12 @@
 const bedrockServer = require('minecraft-bedrock-server')
 
+// The vanilla spawn and packet-dump clients use RakNet, even when BDS defaults to Nethernet.
 module.exports = {
   ...bedrockServer,
   startServerAndWait (version, withTimeout, options) {
-    return bedrockServer.startServerAndWait(version, withTimeout, { ...options, root: __dirname })
+    return bedrockServer.startServerAndWait(version, withTimeout, { transport: 'raknet', ...options, root: __dirname })
   },
   startServerAndWait2 (version, withTimeout, options) {
-    return bedrockServer.startServerAndWait2(version, withTimeout, { ...options, root: __dirname })
+    return bedrockServer.startServerAndWait2(version, withTimeout, { transport: 'raknet', ...options, root: __dirname })
   }
 }
