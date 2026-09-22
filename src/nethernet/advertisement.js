3d1841e79ccd8c37afdda8b55e2cba8b8640d574
extremeheat <extreme@protonmail.ch>
2026-09-22 11:31:47 +0000

    Keep advertisement schemas in bedrock-protocol

--- src/nethernet/advertisement.js
+++ src/nethernet/advertisement.js
@@ -1,6 +1,6 @@
 const { Versions, CURRENT_VERSION } = require('../options')
 const { ProtoDef } = require('protodef')
-const schemas = require('minecraft-data/minecraft-data/data/bedrock/common/nethernetAdvertisement.json')
+const schemas = require('./advertisement.json')
 
 const proto = new ProtoDef(false)
 proto.addTypes(schemas)
